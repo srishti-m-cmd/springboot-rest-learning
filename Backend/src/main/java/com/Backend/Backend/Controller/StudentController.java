1da8e44e851d8c653956de42eb530ee39820c3fd
Srishti <srishtirameshk@gmail.com>
2026-03-05 07:26:42 +0530

    Some changes regarding database

--- Backend/src/main/java/com/Backend/Backend/Controller/StudentController.java
+++ Backend/src/main/java/com/Backend/Backend/Controller/StudentController.java
@@ -1,16 +1,25 @@
 package com.Backend.Backend.Controller;
 
 import com.Backend.Backend.DTO.StudentDTO;
+import com.Backend.Backend.Repository.StudentRepository;
+import com.Backend.Backend.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @GetMapping("/student")
-    public StudentDTO getStudent(){
-        return new StudentDTO(41,"srishti","srishtirameshk@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
     @GetMapping("/student/{id}")
     public StudentDTO getStudentById(){
